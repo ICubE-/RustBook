42fbfddd3ee80e0c0d5f6d74a120e98d96c7e0e3
ICubE- <jehoyeon01@gmail.com>
2024-03-04 21:53:44 +0900

    Add 10.2

---
+++ r10_2_traits_defining_shared_behavior/src/aggregator.rs
@@ -0,0 +1,45 @@
+pub trait Summary {
+    // No default implementation
+    // fn summarize(&self) -> String;
+
+    // Default implementation
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
+
+    fn summarize_author(&self) -> String {
+        self.author.clone()
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
