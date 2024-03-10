f80a88930390c7748bf0bec6a6c70dc3ea3c5e46
ICubE- <jehoyeon01@gmail.com>
2024-03-10 14:41:48 +0900

    Update 10.3

--- r10_3_validating_references_with_lifetimes/src/main.rs
+++ r10_3_validating_references_with_lifetimes/src/main.rs
@@ -1,8 +1,27 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
         y
+    }
+}
+
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
     }
 }
 
@@ -16,4 +35,12 @@ fn main() {
     }
     // error: `string2` does not live long enough
     // println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let s: &'static str = "I have a static lifetime.";
 }
