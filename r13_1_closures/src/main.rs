fbc7ebe372e7606d9bced22d36735ed3c74817f1
ICubE- <jehoyeon01@gmail.com>
2024-03-11 19:51:59 +0900

    Update 13.1

--- r13_1_closures/src/main.rs
+++ r13_1_closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 #[derive(Debug, Clone, Copy)]
 enum ShirtColor {
     Red,
@@ -59,5 +61,37 @@ fn main() {
 
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
-    let only_borrows = || println!("From closure")
+    let only_borrows = || println!("From closure: {:?}", list);
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("Before calling closure: {:?}", list);
+
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    let mut borrows_mutably = || list.push(7);
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }
