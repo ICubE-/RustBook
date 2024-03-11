1e02fdf3a908d0ae5dfd185e7f313b935b34cb40
ICubE- <jehoyeon01@gmail.com>
2024-03-11 12:36:48 +0900

    Update 12.3

--- r12_0_minigrep/src/main.rs
+++ r12_0_minigrep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use r12_0_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,31 +13,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config {query, file_path})
+    if let Err(e) = r12_0_minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
